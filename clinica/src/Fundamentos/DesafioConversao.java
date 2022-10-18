70bfbcea42df44a1fec939a62541df124f2c58cf
lannydev <e.cristina00@gmai.com>
2022-10-17 22:50:55 -0300

    Desafio de conversão

---
+++ clinica/src/Fundamentos/DesafioConversao.java
@@ -0,0 +1,31 @@
+package Fundamentos;
+
+import java.util.Scanner;
+
+public class DesafioConversao {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro salário");
+        String valor1 = sc.next().replace(",", ".");
+
+        System.out.println("Informe o segundo salário");
+        String valor2 = sc.next().replace(",", ".");
+
+        System.out.println("informe o terceiro salário");
+        String valor3 = sc.next().replace(",", ".");
+
+        double salario1 = Double.parseDouble(valor1);
+        double salario2 = Double.parseDouble(valor2);
+        double salario3 = Double.parseDouble(valor3);
+
+
+        double soma = salario1 + salario2 + salario3;
+        double media = soma/3;
+        System.out.println("A média dos salários é "  + media);
+
+        sc.close();
+
+    }
+}
