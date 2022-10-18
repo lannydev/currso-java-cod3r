861fc33d3722d2ac4c5288dff5747b6635da1f43
lannydev <e.cristina00@gmai.com>
2022-10-18 00:42:54 -0300

    Operadores Aritméticos.

---
+++ clinica/src/Fundamentos/OperadoresAritmeticos.java
@@ -0,0 +1,22 @@
+package Fundamentos;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+
+        double x = 2.5;
+        double y = 4.3;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+
+        int a = 8;
+        int b = 6;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * (double) b);
+        System.out.println(a / (double)b);
+    }
+}
